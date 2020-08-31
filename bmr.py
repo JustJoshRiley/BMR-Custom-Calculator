3776fc7250f8c2f785ba81db7993412bfb34d0da
Joshua Riley <Joshy@Gilly-in-Philly.localdomain>
2020-08-31 12:16:22 -0700

    third commit

--- bmr.py
+++ bmr.py
@@ -27,5 +27,21 @@ if gender == 'f' or 'F':
             print(f"Here is your BMR: {fbr}")
         else :
             print({calculate})
-
+else :
+    #ask for male weight
+    male_weight = (input("Enter your Weight in LBS: "))
+    male_weight = float(male_weight)
+    #ask male age
+    male_height = (input("Enter your Height in Inches: "))
+    male_height = float(male_height)
+    #ask male age
+    male_age = (input("Enter you Age: "))
+    male_age = float(male_age)
+    #calculate BMR based on male answer
+    calculate = 'a'
+    if calculate not in 'YyNn':
+        calculate = input("Do you want to Calculate your BMR? Y/N: ")
+        if calculate == 'y' or 'Y':
+            fbr = (66 + (6.2 * male_weight) + (12.7 * male_height) + (6.76 * male_age))
+            print(f"Here is your BRM: {fbr}")
 
