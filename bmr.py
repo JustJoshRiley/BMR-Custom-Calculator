8b7341b5eaa039a797613434db8f8d8864c03617
Joshua Riley <Joshy@Gilly-in-Philly.localdomain>
2020-08-31 12:14:54 -0700

    first commit

---
+++ bmr.py
@@ -0,0 +1,12 @@
+#BMR Calculator
+#description
+print("This Calculator Will Help Determine Your Basal Metabolic Rate (BMR)")
+
+#ask the users gender
+gender = ' '
+while gender not in 'mMfF' or gender == ' ':
+    gender = input("Are you M or F? ")
+    #
+    #print(gender)
+
+
